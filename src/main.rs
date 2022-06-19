01563683da29e6f9da8b847591f53d37a130e529
Ricardo Delfin <me@rdelfin.com>
2022-06-19 19:50:02 +0100

    Added base bevy project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bevy::{input::system::exit_on_esc_system, prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.5, 0.5, 0.5)))
+        .insert_resource(WindowDescriptor {
+            title: "Swing".to_string(),
+            width: 1280.,
+            height: 800.,
+            resizable: false,
+            ..WindowDescriptor::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_system(exit_on_esc_system)
+        .run();
 }
